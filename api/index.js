b8e54423bcf9f7193b6fa398f532be839900eec0
christian.kinate <christian.kinate@FINTRAKSOFWARE.COM>
2024-12-30 15:23:35 +0100

    changesForRenderDeployment

--- api/index.js
+++ api/index.js
@@ -40,11 +40,12 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 //app.use('/api', swaggerUi.serve, swaggerUi.setup(specs));
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
+
+
 
 //middleware
 app.use((err, req, res, next) => {
